3da9ea5787afe4ee3c5fffb42ca07efb4937f76d
Dean <dean.marinov@gmx.at>
2023-01-17 19:01:59 +0100

    Add project files.

---
+++ Controllers/GetPicture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+
+namespace mvc_API.Controllers
+{
+    public class GetPicture : Controller
+    {
+        private string path = @"c:\pictures";
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+        public IActionResult GetPic(string apiKey, string name, string file)
+        {
+            if (apiKey == "getPictures")
+            {
+                name = name + "." + file;
+                if (System.IO.File.Exists(Path.Combine(path, name)))
+                {
+                    Byte[] b = System.IO.File.ReadAllBytes(Path.Combine(path, name));
+                    return File(b, "image/jpg");
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+            }
+            return View();
+        }
+        public IActionResult GetFiles(string apiKey, string req)
+        {
+            if (apiKey == "getPictures" && req == "get")
+            {
+                var files = System.IO.Directory.EnumerateFiles(path).ToList();
+                return Json(files);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
